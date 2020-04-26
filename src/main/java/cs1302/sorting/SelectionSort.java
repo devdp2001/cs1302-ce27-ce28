9dfa1f8040817939d5f6a9debb4290babcdbbe5b
Jack Beasley <jcb12561@uga.edu>
2020-04-26 19:47:31 -0400

    checkpoint-2

--- src/main/java/cs1302/sorting/SelectionSort.java
+++ src/main/java/cs1302/sorting/SelectionSort.java
@@ -12,7 +12,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         Integer[] intArray = {2, 3, 1, 4, 5};
         System.out.println(Arrays.toString(intArray)); // [ 2, 3, 1, 4, 5 ] 
-        selectMin(intArray, 0, 4, Integer::compareTo);
+        selectSort(intArray, 0, 4, Integer::compareTo);
         System.out.println("hi: 4 , lo: 0");
         System.out.println(Arrays.toString(intArray)); // [ 1, 3, 2, 4, 5 ]  
         System.out.println("The swapping of then minimum value in the array to the lo position, "
@@ -20,7 +20,7 @@ public class SelectionSort {
         System.out.println();
         Double[] doubleArray = {1.1, 3.3, 2.2, 4.4, 5.5};
         System.out.println(Arrays.toString(doubleArray)); // [ 1.1, 3.3, 2.2, 4.4, 5.5 ] 
-        selectMin(doubleArray, 1, 4, Double::compareTo);
+        selectSort(doubleArray, 1, 4, Double::compareTo);
         System.out.println("hi: 4, lo: 1");
         System.out.println(Arrays.toString(doubleArray)); // [ 1.1, 2.2, 3.3, 4.4, 5.5 ]  
         System.out.println("The swapping of then minimum value in the array to the lo position, " 
@@ -28,7 +28,7 @@ public class SelectionSort {
         System.out.println();
         String[] stringArray = {"c", "b", "a"};
         System.out.println(Arrays.toString(stringArray));
-        selectMin(stringArray, 0, 2, String::compareTo);
+        selectSort(stringArray, 0, 2, String::compareTo);
         System.out.println("hi: 2, lo: 0");
         System.out.println(Arrays.toString(stringArray));
         System.out.println("The swapping of then minimum value in the array to the lo position, "
@@ -47,17 +47,32 @@ public class SelectionSort {
      */
     public static <T> void selectMin(T[] array, int lo, int hi, Comparator<T> c) {
         T lowest = array[lo];
-        int indexOf = 0; 
+        int mindex = lo; 
         for (int i = lo; i < hi; i++) {
-            if (c.compare(lowest, array[i + 1]) > 0) {
-                lowest = array[i + 1];
-                indexOf = i + 1;
+            if (c.compare(array[mindex], array[i + 1]) > 0) {
+                mindex = i + 1;
             } // if      
         } // for
 
         T temp = array[lo];
-        array[lo] = lowest;
-        array[indexOf] = temp;
+        array[lo] = array[mindex];
+        array[mindex] = temp;
         
-    } // selectMin  
+    } // selectMin
+
+     /**
+     * Method that select sorts an array of a certain type.
+     *
+     * @param <T> types used in array
+     * @param array array that is being sorted
+     * @param lo the lowest index for sorting
+     * @param hi the highest index for sorting
+     * @param c comparator used in method
+     */
+    public static <T> void selectSort(T[] array, int lo, int hi, Comparator<T> c) {
+        for (int i = lo; i < hi; i++) {
+            selectMin(array, i, hi, c);
+        } //for
+    } //selectSort
+
 } // SelectionSort
